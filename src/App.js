c4809b78befe60f9426a5c33f77b8b5924133465
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-04-10 00:32:19 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import './App.css';
 import { useState } from 'react'
+import Todo from './components/Todo';
 
 function App() {
   
-  function addNewTodo(event){
-    event.preventDefault()
-    const todos = document.querySelector('#todos')
-    const todo = document.createElement('p')
-    const input = document.querySelector('#todo')
-    todo.innerHTML = input.value
-    todos.append(todo)
-    input.value = ""
-    console.log(document.querySelector('#todo').value)
-    
-  }
-
-  const [state, setState] = useState({
-    
-  })
   return (
     <div className="App">
-        <h1>Todo List</h1>
-        <p>What do you want to do</p>
-        <form onSubmit={addNewTodo}>
-          <input type="text" id="todo" placeholder="New Todo..."/>
-          <button type="submit">Add</button>
-        </form>
-        <div id="todos"></div>
+      <Todo />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,39 @@
 import styles from './Todo.module.css'
 
-function Todo(props){
+function Todo(){
+
+  function addNewTodo(event){
+    event.preventDefault()
+    if (event.target.value.length() > 0) {
+    const todos = document.querySelector('#todos')
+    const todo = document.createElement('p')
+    const span = document.createElement('span')
+    
+    span.innerHTML = ''
+    todo.className = `${styles.todo}`
+    const input = document.querySelector('#todo')
+    todo.innerHTML = `
+      <p>${input.value}</p>
+    `
+    span.className = styles.removeTodo
+    todo.append(span)
+    todos.append(todo)
+    input.value = ""
+    console.log(document.querySelector('#todo').value)      
+    }
+
+  
+  }
+
   return (
     <>
-      <div>
-        <p>{props.todo}</p>
-        <span className={styles.removeTodo}></span>
-      </div>
+      <h1>Todo List</h1>
+      <p>What do you want to do</p>
+      <form onSubmit={addNewTodo}>
+        <input type="text" id="todo" placeholder="New Todo..." />
+        <button type="submit">Add</button>
+      </form>
+      <div id="todos"></div>
     </>
   )
 }
