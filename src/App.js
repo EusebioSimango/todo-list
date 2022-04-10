3eac6517eadece0f46def0e736286b46aa4c2bb7
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-04-10 13:36:45 +0200

    updating

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import Todo from './components/Todo';
 
 function App() {
   
+  const [todo, setTodo] = useState()
+
+  function addNewTodo() {
+    return 
+      { document.querySelector('.App').append(
+        (
+          <p>{todo}</p>
+        )
+        )
+      }
+    
+  }
+  
   return (
     <div className="App">
-      <Todo />
+      <h1>Todo List</h1>
+      <p>What do you want to do</p>
+      <Todo setTodo={setTodo} addNewTodo={addNewTodo}/>
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,40 +1,12 @@
 import styles from './Todo.module.css'
 
-function Todo(){
-
-  function addNewTodo(event){
-    event.preventDefault()
-    if (event.target.value.length() > 0) {
-    const todos = document.querySelector('#todos')
-    const todo = document.createElement('p')
-    const span = document.createElement('span')
-    
-    span.innerHTML = ''
-    todo.className = `${styles.todo}`
-    const input = document.querySelector('#todo')
-    todo.innerHTML = `
-      <p>${input.value}</p>
-    `
-    span.className = styles.removeTodo
-    todo.append(span)
-    todos.append(todo)
-    input.value = ""
-    console.log(document.querySelector('#todo').value)      
-    }
-
-  
-  }
+function Todo({ setTodo, addNewTodo }){
 
   return (
-    <>
-      <h1>Todo List</h1>
-      <p>What do you want to do</p>
-      <form onSubmit={addNewTodo}>
-        <input type="text" id="todo" placeholder="New Todo..." />
-        <button type="submit">Add</button>
-      </form>
-      <div id="todos"></div>
-    </>
+    <div className={styles.controler}>
+      <input id="todo" placeholder="New Todo..." onChange={e => setTodo(e.target.value)}/>
+      <button onClick={addNewTodo}>Add</button>
+    </div>
   )
 }
 
